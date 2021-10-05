9974d9da60bc4a619f55ebfd964ea78b5d4d6c9e
hilalsidhic <hilalsidhic21@gmail.com>
2021-10-05 07:05:24 +0530

    project completed

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import './RowPost.css'
 import axios from '../../axios'
-import { image_w500} from '../../constants/constants'
-
+import { API_KEY, image_w500} from '../../constants/constants'
+import Youtube from 'react-youtube'
 
 function RowPost(props) {
+    const [ytMovie, setytMovie] = useState()
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1
+        },};
     const [rowMovies, setrowMovies] = useState([])
     useEffect(() => {     
         axios.get(props.genre).then((response) =>{
@@ -13,17 +21,32 @@ function RowPost(props) {
         }).catch((error) =>{
             console.log(error)
         })
-    }, [])
+    }, []);
+    const movieHandler = (id) => {
+        axios.get(`/movie/${id}/videos?language=en-US&api_key=${API_KEY}`).then((response) =>{
+            console.log(response.data)
+            if(response.data.results.length !== 0){
+                setytMovie(response.data.results[0].key)
+            }
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
     return (
         <div className="row">
             <h2>{props.title}</h2>
             <div className="posters">
                 {rowMovies.map((obj) =>{
                     return(
-                          <img className={props.small ? "small-poster" : "poster"} src={image_w500 + obj.backdrop_path} alt="poster" />
+                          <img onClick={()=>{movieHandler(obj.id)}} className={props.small ? "small-poster" : "poster"} src={image_w500 + obj.backdrop_path}  alt="poster" />
                     )
                 })}
             </div>
+            {ytMovie &&
+            <div id="youtube">
+                <Youtube videoId={ytMovie} opts={opts}  />
+            </div> }
+            
         </div>
     )
 }
--- src/Urls.js
+++ src/Urls.js
@@ -2,12 +2,14 @@ import { API_KEY } from "./constants/constants"
 
 export const originals=`/discover/tv?api_key=${API_KEY}&with_networks=213`
 
-export const action=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
+export const action=`/discover/movie?api_key=${API_KEY}&with_genres=28`
 
-export const comedy=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`
+export const comedy=`/discover/movie?api_key=${API_KEY}&with_genres=35`
 
-export const horror=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`
+export const horror=`/discover/movie?api_key=${API_KEY}&with_genres=27`
 
-export const romance=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const romance=`/discover/movie?api_key=${API_KEY}&with_genres=10749`
 
-export const documentaries=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`
+export const documentaries=`/discover/movie?api_key=${API_KEY}&with_genres=99`
+
+export const ytId='https://api.themoviedb.org/3/movie/379686/videos?language=en-US&api_key=fe7618a98c8eaed5bf4a020ddb974602'
